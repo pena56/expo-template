18c79d0134e612afc0f594d958f4d3c5eec93d48
moses <ogbopinamoses@gmail.com>
2024-07-08 10:21:43 +0100

    chore(setup): setup expo and it's initial configuration

---
+++ src/app/index.tsx
@@ -0,0 +1,9 @@
+import { Text, View } from 'react-native';
+
+export default function Screen() {
+	return (
+		<View className="flex-1 items-center justify-center bg-[#F1F1F1]">
+			<Text className="text-center text-3xl">Screen</Text>
+		</View>
+	);
+}
